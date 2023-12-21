b207acd05c0a3ed083eebf876c795f4b501cc368
Victoria <133873038+Vi-Sad@users.noreply.github.com>
2023-12-21 17:07:24 +0300

    + Homework #21
    + git.docx
    + homework_21.py
    + HW21.docx

---
+++ homework-21/homework_21.py
@@ -0,0 +1,15 @@
+print('- ' * 80)
+
+from colorama import Fore
+
+# С Новым годом!
+
+print('      *' * 25 + '\n' + '   *   ' * 25 + '\n' + '*      ' * 25)
+print('   *   ' * 10 + Fore.BLUE + '| HAPPY NEW YEAR! |' + Fore.RESET + '   *   ' * 10)
+print('*      ' * 11 + Fore.RED + '   *' + Fore.RESET + '      *' * 11 + '\n' + Fore.RESET + '   *   ' * 11
+      + Fore.GREEN + '   ^' + Fore.RESET + '   *   ' * 11 + '\n' + Fore.RESET + '*      ' * 11 + Fore.GREEN + '  / \\'
+      + Fore.RESET + '      *' * 11 + '\n' + Fore.RESET + '   *   ' * 11 + Fore.GREEN + ' /   \\'
+      + Fore.RESET + '   *   ' * 11 + '\n' + Fore.RESET + '*      ' * 11 + Fore.GREEN + '/     \\'
+      + Fore.RESET + '   *   ' * 11)
+
+print(Fore.RESET + '- ' * 80)
